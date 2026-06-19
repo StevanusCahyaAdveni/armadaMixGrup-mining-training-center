440493f6660c7d8ecd4e14cfd8da60667f0e6c99
Stevanus Cahya Adveni <evanlum4jang@gmail.com>
2026-06-19 23:09:48 +0700

    feat: implement employee management and automated timesheet/overtime calculation system

--- actions/pages/employee/employees.php
+++ actions/pages/employee/employees.php
@@ -5,24 +5,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['addData'])) {
         $id = generate_uuid();
         $full_name = sani($_POST['full_name']);
-        $company_name = sani($_POST['company_name']);
         $position = sani($_POST['position']);
-        $employee_id = sani($_POST['employee_id']);
-        $bpjs_tenaker_no = sani($_POST['bpjs_tenaker_no']);
-        $bpjs_kes_no = sani($_POST['bpjs_kes_no']);
-        $merdeka_sehat = sani($_POST['merdeka_sehat']);
-        $mine_permit = sani($_POST['mine_permit']);
-        $induction_schedule = sani($_POST['induction_schedule']);
-        $mine_permit_scm = sani($_POST['mine_permit_scm']);
-        $simper_teory_test = sani($_POST['simper_teory_test']);
-        $simper_practice_test = sani($_POST['simper_practice_test']);
-        $simper_ojt = sani($_POST['simper_ojt']);
-        $simper_status = sani($_POST['simper_status']);
-        $gaji_pokok = sani($_POST['gaji_pokok']);
-        $tunjangan_tetap = sani($_POST['tunjangan_tetap']);
-        $query = "INSERT INTO employees (id, full_name, company_name, position, employee_id, bpjs_tenaker_no, bpjs_kes_no, merdeka_sehat, mine_permit, induction_schedule, mine_permit_scm, simper_teory_test, simper_practice_test, simper_ojt, simper_status, gaji_pokok, tunjangan_tetap) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $params = [$id, $full_name, $company_name, $position, $employee_id, $bpjs_tenaker_no, $bpjs_kes_no, $merdeka_sehat, $mine_permit, $induction_schedule, $mine_permit_scm, $simper_teory_test, $simper_practice_test, $simper_ojt, $simper_status, $gaji_pokok, $tunjangan_tetap];
-        $types = 'sssssssssssssssss';
+        $join_date = sani($_POST['join_date']) ?: null;
+        $query = "INSERT INTO employees (id, full_name, position, join_date) VALUES (?, ?, ?, ?)";
+        $params = [$id, $full_name, $position, $join_date];
+        $types = 'ssss';
         $insertResult = executeSecure($con, $query, $params, $types);
 
         if ($insertResult) {
@@ -42,24 +29,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['updateData'])) {
         $id = sani($_POST['id']);
         $full_name = sani($_POST['full_name']);
-        $company_name = sani($_POST['company_name']);
         $position = sani($_POST['position']);
-        $employee_id = sani($_POST['employee_id']);
-        $bpjs_tenaker_no = sani($_POST['bpjs_tenaker_no']);
-        $bpjs_kes_no = sani($_POST['bpjs_kes_no']);
-        $merdeka_sehat = sani($_POST['merdeka_sehat']);
-        $mine_permit = sani($_POST['mine_permit']);
-        $induction_schedule = sani($_POST['induction_schedule']);
-        $mine_permit_scm = sani($_POST['mine_permit_scm']);
-        $simper_teory_test = sani($_POST['simper_teory_test']);
-        $simper_practice_test = sani($_POST['simper_practice_test']);
-        $simper_ojt = sani($_POST['simper_ojt']);
-        $simper_status = sani($_POST['simper_status']);
-        $gaji_pokok = sani($_POST['gaji_pokok']);
-        $tunjangan_tetap = sani($_POST['tunjangan_tetap']);
-        $query = "UPDATE employees SET full_name = ?, company_name = ?, position = ?, employee_id = ?, bpjs_tenaker_no = ?, bpjs_kes_no = ?, merdeka_sehat = ?, mine_permit = ?, induction_schedule = ?, mine_permit_scm = ?, simper_teory_test = ?, simper_practice_test = ?, simper_ojt = ?, simper_status = ?, gaji_pokok = ?, tunjangan_tetap = ? WHERE id = ?";
-        $params = [$full_name, $company_name, $position, $employee_id, $bpjs_tenaker_no, $bpjs_kes_no, $merdeka_sehat, $mine_permit, $induction_schedule, $mine_permit_scm, $simper_teory_test, $simper_practice_test, $simper_ojt, $simper_status, $gaji_pokok, $tunjangan_tetap, $id];
-        $types = 'sssssssssssssssss';
+        $join_date = sani($_POST['join_date']) ?: null;
+        $query = "UPDATE employees SET full_name = ?, position = ?, join_date = ? WHERE id = ?";
+        $params = [$full_name, $position, $join_date, $id];
+        $types = 'ssss';
         $updateResult = executeSecure($con, $query, $params, $types);
 
         if ($updateResult) {
